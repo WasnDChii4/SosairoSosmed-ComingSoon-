e9c656c7cf4dcc9b238efd0db410ed35a6212eec
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-05-22 19:52:42 +0700

    Update fitur

--- frontend-sosairo/src/Pages/SettingsBar/MyProfile.jsx
+++ frontend-sosairo/src/Pages/SettingsBar/MyProfile.jsx
@@ -70,6 +70,16 @@ export default function MyProfile() {
                 <label className="label">About Me</label>
                 <textarea type="text" value={user.about_me} className="textarea w-full" disabled />
               </div>
+              <button className="btn w-full" onClick={()=>document.getElementById('my_modal_3').showModal()}>Edit</button>
+              <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                  <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                  </form>
+                  <h3 className="font-bold text-lg">Hello!</h3>
+                  <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                </div>
+              </dialog>
             </div>
           </>
         ) : (
