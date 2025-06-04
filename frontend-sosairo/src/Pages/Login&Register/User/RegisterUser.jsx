3142e8915b0a37d463754b531dc0494106b18ec0
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-06-04 19:35:25 +0700

    update fitur

--- frontend-sosairo/src/Pages/Login&Register/User/RegisterUser.jsx
+++ frontend-sosairo/src/Pages/Login&Register/User/RegisterUser.jsx
@@ -36,7 +36,7 @@ export default function RegisterUser() {
       
       Swal.fire({
         icon: 'success',
-        title: 'Registrasi Berhasil!',
+        title: 'Registrasi Berhasil!dwdaw',
         text: `Selamat datang, ${response.data.user.name}!`,
         confirmButtonText: 'OK'
       }).then(() => {
