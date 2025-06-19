3b3e91c9222d3efade5248690ff747b286c4cb8e
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-06-19 13:52:15 +0700

    kerapian code

--- frontend-sosairo/src/Layout/Friends/FriendsLayoutPage.jsx
+++ frontend-sosairo/src/Layout/Friends/FriendsLayoutPage.jsx
@@ -6,19 +6,13 @@ import FriendsSidebarLeft from "./FriendsSidebarLeft";
 export default function FriendsLayoutPage() {
   return (
     <div className="flex flex-col overflow-hidden">
-      {/* Navbar fixed height */}
       <div className="h-16 flex-shrink-0">
         <FriendsNavbarTop />
       </div>
-
-      {/* Sisa layar = 100vh - 4rem (tinggi navbar) */}
       <div className="flex h-[calc(90vh-4rem)] overflow-hidden">
-        {/* Sidebar scrollable */}
         <aside className="w-64 flex-shrink-0">
           <FriendsSidebarLeft />
         </aside>
-
-        {/* Main content scrollable */}
         <main className="flex-1 p-4">
           <Outlet />
         </main>
