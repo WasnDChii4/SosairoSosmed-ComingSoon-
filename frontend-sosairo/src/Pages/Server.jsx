9895143e0c8de55f1200f302d7f600fc0e715463
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-06-23 16:43:15 +0700

    pembenaran layout

--- frontend-sosairo/src/Pages/Server.jsx
+++ frontend-sosairo/src/Pages/Server.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ServerSidebarLeft from "../Layout/Server/ServerSidebarLeft";
-import ServerChat from "../Layout/Server/ServerChat";
 
 export default function Server() {
   return (
     <div className="flex h-screen">
       <ServerSidebarLeft />
-      <ServerChat />
     </div>
   );
 }
