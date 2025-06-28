32d310e276befc8ad0a3979462ffb3abed8cd14a
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-06-28 18:40:47 +0700

    Update fitur

--- backendSosairo/database/migrations/2025_04_15_113942_create_categories_table.php
+++ backendSosairo/database/migrations/2025_04_15_113942_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('server_id')->constrained('servers')->onDelete('cascade');
+            $table->foreignId('server_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
         });
