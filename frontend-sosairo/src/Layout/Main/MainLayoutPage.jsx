bf552fde44b974bd2894744ac5ddcf4f92e10a48
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-08-13 13:39:20 +0700

    Update code

--- frontend-sosairo/src/Layout/Main/MainLayoutPage.jsx
+++ frontend-sosairo/src/Layout/Main/MainLayoutPage.jsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router-dom";
-// import MainTopNavbar from "./MainTopNavbar";
 import MainSidebarLeft from "./MainSidebarLeft";
 
 export default function MainLayoutPage() {
   return (
     <div className="h-screen flex flex-col overflow-hidden">
-      {/* <div className="h-14 flex-shrink-0">
-        <MainTopNavbar />
-      </div> */}
       <div className="flex flex-1 overflow-hidden">
         <div className="w-20 h-full flex-shrink-0 overflow-y-auto">
           <MainSidebarLeft />
